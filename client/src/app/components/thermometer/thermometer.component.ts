3a4de12a25e81b9ef86e6e2d1f8f5d3da21f0210
changer25 <57739331+changer25@users.noreply.github.com>
2024-02-25 21:57:11 -0800

    finished part 5

--- client/src/app/components/thermometer/thermometer.component.ts
+++ client/src/app/components/thermometer/thermometer.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { TrackFeature } from '../../data/track-feature';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-thermometer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './thermometer.component.html',
   styleUrl: './thermometer.component.scss'
 })
 
 export class ThermometerComponent implements OnInit {
   //TODO: define Input fields and bind them to the template.
-
+  @Input() feature: TrackFeature | undefined;
   constructor() { }
 
   ngOnInit() {
