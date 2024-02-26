35dc27eb8528a39d807ac774652fb50fba932964
changer25 <57739331+changer25@users.noreply.github.com>
2024-02-25 21:11:10 -0800

    finished part 4

--- client/src/app/pages/album-page/album-page.component.ts
+++ client/src/app/pages/album-page/album-page.component.ts
@@ -4,11 +4,13 @@ import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
 import { CommonModule } from '@angular/common';
+import { SpotifyService } from '../../services/spotify.service';
+import { TrackListComponent } from '../../components/track-list/track-list.component';
 
 @Component({
   selector: 'app-album-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TrackListComponent],
   templateUrl: './album-page.component.html',
   styleUrl: './album-page.component.scss'
 })
@@ -19,11 +21,24 @@ export class AlbumPageComponent implements OnInit {
 	tracks:TrackData[] | undefined;
 
 
-  constructor(private route: ActivatedRoute) { }
-
+  constructor(
+    private route: ActivatedRoute,
+    private spotifyService: SpotifyService // Inject SpotifyService
+  ) { }
+  
   ngOnInit() {
   	this.albumId = this.route.snapshot.paramMap.get('id') || "";
   	//TODO: inject spotifyService and use it to get the album data and the tracks for the album
+    if (this.albumId) {
+    this.spotifyService.getAlbum(this.albumId).then(albumData => {
+      this.album = albumData;
+    }).catch(error => console.error("Error fetching album data:", error));
+
+    this.spotifyService.getTracksForAlbum(this.albumId).then(tracksData => {
+      this.tracks = tracksData;
+    }).catch(error => console.error("Error fetching album tracks:", error));
+
+    }
   }
 
 }
